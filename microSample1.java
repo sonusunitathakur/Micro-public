a18237cac17ddabcf10c78fb6016e1cb139d53fa
sonusunitathakur <80622120+sonusunitathakur@users.noreply.github.com>
2022-02-13 23:24:55 +0530

    Add files via upload

---
+++ microSample1.java
@@ -0,0 +1,26 @@
+package Sample1;
+
+public class microSample1
+{
+    
+    public void return1()
+	{
+		System.out.println("Hi!this is sunita");
+	}
+    class innSample1
+    {
+    	
+    	public void return2()
+    	{
+    		
+    	System.out.println("Hi! i am back");
+    	}
+
+		
+		
+    }
+    
+    
+	
+
+}
---
+++ microSample2.java
@@ -0,0 +1,19 @@
+package Sample1;
+
+public class microSample2 {
+
+	public static void main(String[] args) 
+	{
+		
+		//	microSample1 ms=new microSample1();
+		//	ms.return1();
+			
+			microSample1.innSample1 IS=new microSample1().new innSample1();
+			IS.return2();
+			
+	           
+	
+
+	}
+
+}
